0b5ca12feaf2a07d27144c27d94a321eb5a7f675
Jiselle Martins <jiselle.santana@gfxconsultoria.com>
2021-02-10 14:57:18 -0300

    adicionando lógica de cálculo

--- app/src/main/java/com/jisellemartins/alcoolgasolina/MainActivity.kt
+++ app/src/main/java/com/jisellemartins/alcoolgasolina/MainActivity.kt
@@ -10,10 +10,32 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
     }
 
-    fun btCalculcar(view: View){
+    fun btCalculcar(view: View) {
         val precoAlcool = etAlcool.text.toString()
         val precoGasolina = etGasolina.text.toString()
+
+        if(validarCampos(precoAlcool, precoGasolina)) calcularMelhorPreco(precoAlcool, precoGasolina)
+        else tvResult.setText("Preencha todos os campos primeiro")
+
+    }
+
+    fun validarCampos(precoAlcool: String, precoGasolina: String) : Boolean {
+        return !(precoAlcool == null || precoAlcool.equals("") || precoGasolina == null || precoGasolina.equals(""))
+    }
+
+    fun calcularMelhorPreco(precoAlcool: String, precoGasolina: String) {
+
+        val valorAlcool = precoAlcool.toDouble()
+        val valorGasolina = precoGasolina.toDouble()
+
+        val resultadoPreco = valorAlcool / valorGasolina
+
+
+        if (resultadoPreco >= 0.7) tvResult.setText("Melhor utilizar Gasolina")
+        else tvResult.setText("Melhor utilizar Álcool")
+
     }
 }
